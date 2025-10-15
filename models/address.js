9db1d9be3e66ee229802f4f5cf0dcadcc04b8dfa
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:52:07 +0200

    Create address.js

---
+++ models/address.js
@@ -0,0 +1,52 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.users, {
+        foreignKey: "userId",
+        targetKey: "userId"
+      });
+    }
+  }
+  address.init(
+    {
+      addressId: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4
+      },
+      userId: DataTypes.UUID,
+      latitude: DataTypes.DECIMAL,
+      longitude: DataTypes.DECIMAL,
+      accuracy: DataTypes.DECIMAL,
+      streetNumber: DataTypes.STRING,
+      streetName: DataTypes.STRING,
+      suburb: DataTypes.STRING,
+      city: DataTypes.STRING,
+      province: DataTypes.STRING,
+      country: DataTypes.STRING,
+      postalCode: DataTypes.STRING,
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      sequelize,
+      modelName: "address",
+      timestamps: true
+    }
+  );
+  return address;
+};
