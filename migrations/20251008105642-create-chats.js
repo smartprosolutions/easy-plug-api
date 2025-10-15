e7b1e9836c3911ede40bb796c3403a07ab27ee46
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:52:16 +0200

    Create 20251008105642-create-chats.js

---
+++ migrations/20251008105642-create-chats.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('chats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      chatId: {
+        type: Sequelize.INTEGER
+      },
+      listingId: {
+        type: Sequelize.UUID
+      },
+      buyerId: {
+        type: Sequelize.UUID
+      },
+      sellerId: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('chats');
+  }
+};
