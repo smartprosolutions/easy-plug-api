02e40bfa50fe153fcc08d265eb2a6b2bec41c2a9
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:53:47 +0200

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const authRoutes = require('./auth');
+const subscriptionsRoutes = require('./subscriptions');
+const usersRoutes = require('./users');
+const listingsRoutes = require('./listings');
+const sellerInfoRoutes = require('./sellerInfo');
+const sellerSubscriptionRoutes = require('./sellerSubscription');
+const ratingsRoutes = require('./ratings');
+const transactionsRoutes = require('./transactions');
+const paymentsRoutes = require('./payments');
+const chatsRoutes = require('./chats');
+const chatMessagesRoutes = require('./chatMessages');
+const addressRoutes = require('./address');
+
+router.use('/auth', authRoutes);
+router.use('/subscriptions', subscriptionsRoutes);
+router.use('/users', usersRoutes);
+router.use('/listings', listingsRoutes);
+router.use('/seller-info', sellerInfoRoutes);
+router.use('/seller-subscriptions', sellerSubscriptionRoutes);
+router.use('/ratings', ratingsRoutes);
+router.use('/transactions', transactionsRoutes);
+router.use('/payments', paymentsRoutes);
+router.use('/chats', chatsRoutes);
+router.use('/chat-messages', chatMessagesRoutes);
+router.use('/addresses', addressRoutes);
+
+module.exports = router;
