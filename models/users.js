84a359696eaeae9e2f5fd6f74d2f7047b12cc17d
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:51:32 +0200

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,93 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // a user can have many listings (sellerId on listings stores user.userId)
+      this.hasMany(models.listings, {
+        foreignKey: "sellerId",
+        sourceKey: "userId"
+      });
+      // a user may have seller info
+      this.hasOne(models.sellerInfo, {
+        foreignKey: "userId",
+        sourceKey: "userId"
+      });
+      // a user can have many addresses
+      this.hasMany(models.address, {
+        foreignKey: "userId",
+        sourceKey: "userId"
+      });
+      // transactions as buyer and seller
+      this.hasMany(models.transactions, {
+        foreignKey: "buyerId",
+        sourceKey: "userId",
+        as: "purchases"
+      });
+      this.hasMany(models.transactions, {
+        foreignKey: "sellerId",
+        sourceKey: "userId",
+        as: "sales"
+      });
+    }
+  }
+  users.init(
+    {
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      idNumber: {
+        type: DataTypes.STRING,
+        unique: true
+      },
+      profilePicture: {
+        type: DataTypes.STRING,
+      },
+      passwordHash: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: DataTypes.STRING,
+      },
+      status: {
+        type: DataTypes.STRING,
+      },
+      userType: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: "users",
+      timestamps: true
+    }
+  );
+  return users;
+};
