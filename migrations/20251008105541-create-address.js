dffec80c7e013af88529ce80a9acbe1946b18718
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:52:27 +0200

    Create 20251008105541-create-address.js

---
+++ migrations/20251008105541-create-address.js
@@ -0,0 +1,58 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("addresses", {
+      addressId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      userId: {
+        type: Sequelize.UUID
+      },
+      latitude: {
+        type: Sequelize.DECIMAL
+      },
+      longitude: {
+        type: Sequelize.DECIMAL
+      },
+      accuracy: {
+        type: Sequelize.DECIMAL
+      },
+      streetNumber: {
+        type: Sequelize.STRING
+      },
+      streetName: {
+        type: Sequelize.STRING
+      },
+      suburb: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      province: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("addresses");
+  }
+};
