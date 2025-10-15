1e9786f73d473e321037b85fca617b4ba3f936df
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:52:02 +0200

    Create chatmessages.js

---
+++ models/chatmessages.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class chatMessages extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.chats, {
+        foreignKey: "chatId",
+        targetKey: "chatId"
+      });
+      this.belongsTo(models.users, {
+        foreignKey: "senderId",
+        targetKey: "userId",
+        as: "sender"
+      });
+      this.belongsTo(models.users, {
+        foreignKey: "receiverId",
+        targetKey: "userId",
+        as: "receiver"
+      });
+    }
+  }
+  chatMessages.init(
+    {
+      messageId: DataTypes.INTEGER,
+      chatId: DataTypes.INTEGER,
+      senderId: DataTypes.UUID,
+      receiverId: DataTypes.UUID,
+      message: DataTypes.TEXT,
+      imaages: DataTypes.ARRAY(DataTypes.TEXT),
+      isRead: DataTypes.BOOLEAN,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE
+    },
+    {
+      sequelize,
+      modelName: "chatMessages"
+    }
+  );
+  return chatMessages;
+};
