678fa489bc57e054a9aa6dc978e8e1130bdfbc14
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:52:37 +0200

    Create error.js

---
+++ middleware/error.js
@@ -0,0 +1,8 @@
+function ErrorMid(err, req, res, next) {
+  console.error(err);
+  res
+    .status(err.status || 500)
+    .json({ success: false, message: err.message || "Server Error" });
+}
+
+module.exports = ErrorMid;
