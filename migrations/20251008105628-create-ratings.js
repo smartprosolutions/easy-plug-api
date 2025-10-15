fbb4bea3eecbc4e81f99c3fdcaa940ae71d72dd6
Kamzen <37660966+Kamzen@users.noreply.github.com>
2025-10-15 20:52:18 +0200

    Create 20251008105628-create-ratings.js

---
+++ migrations/20251008105628-create-ratings.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ratings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ratingId: {
+        type: Sequelize.INTEGER
+      },
+      sellerId: {
+        type: Sequelize.UUID
+      },
+      userId: {
+        type: Sequelize.UUID
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ratings');
+  }
+};
