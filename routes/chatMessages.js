56672c55e913c1342197728099243bd4840abe9e
Kamzen <37660966+Kamzen@users.noreply.github.com>
2026-02-17 19:39:16 +0200

    Update chatMessages.js

--- routes/chatMessages.js
+++ routes/chatMessages.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 
 router.post('/', auth, sendMessage);
 router.get('/:chatId', auth, listMessages);
+router.post('/chat/:chatId/read', auth, markAsRead);
 router.post('/:id/read', auth, markAsRead);
 
 module.exports = router;
