192c1e49dca966d9c6d37424694767d1fe4b4388
Kamzen <37660966+Kamzen@users.noreply.github.com>
2026-02-19 08:04:19 +0200

    Update address.js

--- routes/address.js
+++ routes/address.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { listAddresses, createAddress, getMyAddress } = require('../controllers/addressController');
-const auth = require('../middleware/auth');
+const {
+  listAddresses,
+  createAddress,
+  createAddressByBrowser,
+  getMyAddress,
+} = require("../controllers/addressController");
+const auth = require("../middleware/auth");
 
-router.get('/', listAddresses);
-router.post('/', auth, createAddress);
-router.get('/me', auth, getMyAddress);
+router.get("/", listAddresses);
+router.post("/", auth, createAddress);
+router.post("/browser", createAddressByBrowser);
+router.get("/me", auth, getMyAddress);
 
 module.exports = router;
